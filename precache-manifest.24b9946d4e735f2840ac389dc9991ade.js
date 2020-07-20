41c5bb61f0b5e162e5fea5946ef1e6672100510f
shanks2305 <contactshishirbahuguna@gmail.com>
2020-07-20 23:07:42 +0530

    Updates

---
+++ precache-manifest.24b9946d4e735f2840ac389dc9991ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be34f6b9434db01b7345f44f33fa93f7",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "656c023f80487083e3fe",
+    "url": "/blog/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b9160903bbf061c5d12b",
+    "url": "/blog/static/css/main.ba3b59bb.chunk.css"
+  },
+  {
+    "revision": "656c023f80487083e3fe",
+    "url": "/blog/static/js/2.fb579ea7.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/blog/static/js/2.fb579ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9160903bbf061c5d12b",
+    "url": "/blog/static/js/main.749f18be.chunk.js"
+  },
+  {
+    "revision": "042e0e1222fb6f3edc08",
+    "url": "/blog/static/js/runtime-main.1dc37962.js"
+  }
+]);
